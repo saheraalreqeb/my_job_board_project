00eb80cd1cc06d286568c9db0a2f085842602a05
saheraalreqeb <s.a.alreqeb@gmail.com>
2022-09-17 12:09:39 +0300

    adding job html templates, and views

--- job/views.py
+++ job/views.py
@@ -3,7 +3,9 @@ from .models import Job
 # Create your views here.
 
 def job_list(request):
-    pass
+    job_list = Job.objects.all()
+    context = {'jobs',job_list}
+    return render(request, "job_list.html", context)
 
 def job_detail(request, pk):
     pass
